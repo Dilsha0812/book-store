ccd60adb3413f94d2b9d5927a9251dd33e82c9bc
Dilsha0812 <dilshasalamkp@gmail.com>
2024-11-24 12:57:06 +0530

    Changed the login styling

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div className='h-[calc(100vh-120px)] flex justify-center items-center'>
+    <div className='h-[calc(100vh-120px)] flex justify-center '>
         <div>
             <h2 className='text-xl font-semibold'>Please Login</h2>
         </div>
