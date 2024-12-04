3822adf78c4db1c0685725c0e75b9eaa51981a87
Dilsha0812 <dilshasalamkp@gmail.com>
2024-12-04 17:48:27 +0530

    checkout page updated

--- frontend/src/pages/books/CheckOutPage.jsx
+++ frontend/src/pages/books/CheckOutPage.jsx
@@ -157,7 +157,7 @@ const CheckOutPage = () => {
                                         <input 
                                         onChange={(e)=>setIsChecked(e.target.checked)}
                                         type="checkbox" name="billing_same" id="billing_same" className="form-checkbox" />
-                                        <label htmlFor="billing_same" className="ml-2 ">I am aggree to the <Link className='underline underline-offset-2 text-blue-600'>Terms & Conditions</Link> and <Link className='underline underline-offset-2 text-blue-600'>Shoping Policy.</Link></label>
+                                        <label htmlFor="billing_same" className="ml-2 ">I agree to the <Link className='underline underline-offset-2 text-blue-600'>Terms & Conditions</Link> and <Link className='underline underline-offset-2 text-blue-600'>Shoping Policy.</Link></label>
                                     </div>
                                 </div>
                                 <div className="md:col-span-5 text-right">
